412a95c894a53f5e60f8da5f7a30a1d520b96351
ferbcn <ferbcn@pm.me>
2024-09-05 11:55:36 +0200

    vanilla frontend

---
+++ ProjectMonitor/static/dashboard.js
@@ -0,0 +1,35 @@
+async function fetchData() {
+    const response = await fetch('/dashboard-data');
+    const data = await response.json();
+    const tableBody = document.getElementById('data-table');
+    tableBody.innerHTML = ''; // Clear existing table data
+    data.forEach(site => {
+        const row = document.createElement('tr');
+        row.style.backgroundColor = 'darkgreen';
+        
+        row.innerHTML = `
+            <td>${site.name}</td>
+            <td>${site.url}</td>
+            <td>${site.up ? 'Up' : 'Down'}</td>
+            <td>${site.ping_time}</td>
+            <td>${site.downloadMillis}</td>
+        `;
+        if (site.downloadMillis > 99) {
+            row.style.backgroundColor = 'orange';
+        }
+        
+        if (!site.up) {
+            row.style.backgroundColor = 'firebrick';
+        } 
+        
+        tableBody.appendChild(row);
+    });
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetchData();
+});
+
+document.getElementById('refresh').addEventListener('click', () => {
+    fetchData();
+});
